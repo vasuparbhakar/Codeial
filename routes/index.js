b43280b7d3ab8c0c86e0c03794fdefd9579afed8
vasuparbhakar <77485965+vasuparbhakar@users.noreply.github.com>
2022-07-26 18:20:48 +0530

    Setting up ejs

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const homeController = require("../controllers/home_controller");
 
 router.get("/", homeController.home);
+router.use("/users", require("./users"));
+
+// for any further routes, access from here
+// router.use('/routerName',require('./routerfile'));
 
 module.exports = router;
