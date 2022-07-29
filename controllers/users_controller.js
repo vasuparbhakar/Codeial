7dcf45fe72a5a5bbed8bf492e1e9bdc2bba78ebf
vasuparbhakar <77485965+vasuparbhakar@users.noreply.github.com>
2022-07-29 13:12:28 +0530

    Render Pages for Sign in and Sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,20 @@
+// render the user profile Page
 module.exports.profile = function (req, res) {
   return res.render("user_profile", {
     title: "User Profile",
+  });
+};
+
+// render the sign in Page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Sign UP",
+  });
+};
+
+// render the sign up Page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Sign IN",
   });
 };
