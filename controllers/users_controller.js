f138955d236375ee69ab848f776583d2b2f2aa81
vasuparbhakar <77485965+vasuparbhakar@users.noreply.github.com>
2022-07-31 15:32:21 +0530

    Create Sign Out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -59,3 +59,12 @@ module.exports.create = function (req, res) {
 module.exports.createSession = function (req, res) {
   return res.redirect("/");
 };
+
+module.exports.destroySession = function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return err;
+    }
+  });
+  return res.redirect("/");
+};
